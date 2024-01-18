046bd5300f3aad86c8c7d38e8c034e208c32312f
Foximus Prime 3695 - SYS01 <2707036@y115.org>
2024-01-17 19:54:38 -0600

    set up encoders and turn / speed inhibitors

--- Robot-Code/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -6,13 +6,25 @@ import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import com.revrobotics.RelativeEncoder;
+
 
 
 public class DriveTrain extends SubsystemBase{
 
+// set up encoders for when we need to access them (currently unused)
+    public static RelativeEncoder leftMasterEncoder;
+    public static RelativeEncoder leftSlaveEncoder;
+    public static RelativeEncoder rightMasterEncoder;
+    public static RelativeEncoder rightSlaveEncoder;
+
     CommandXboxController controller;
     double rightTrigger;
     double leftTrigger;
+
+    // replace these later when we have a constants file to place these variables in.
+    double turnSpeedInhibitor = 0.3;
+    double speedInhibitor = 0.3;
 
 
     public static CANSparkMax leftMaster;
@@ -21,27 +33,26 @@ public class DriveTrain extends SubsystemBase{
     public static CANSparkMax rightSlave;
 
 
-
-
     public void Drive() {
 
-   
+   //getting encoder values: 
+    leftMasterEncoder = leftMaster.getEncoder();
+    leftSlaveEncoder = leftMaster.getEncoder();
+    rightMasterEncoder = rightMaster.getEncoder();
+     rightSlaveEncoder = rightSlave.getEncoder();
+
 
 
     double leftSpeed = 0.3;
     double rightSpeed = 0.3;
 
-
     leftSpeed = MathUtil.clamp(leftSpeed, -1, 1);
     rightSpeed = MathUtil.clamp(rightSpeed, -1, 1);
-
 
     leftMaster.set(leftSpeed);
     leftSlave.set(leftSpeed);
     rightMaster.set(rightSpeed);
     rightSlave.set(rightSpeed);
-
-
 
     leftMaster = new CANSparkMax(1, com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
     leftSlave = new CANSparkMax(2, com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
@@ -49,19 +60,9 @@ public class DriveTrain extends SubsystemBase{
     rightSlave = new CANSparkMax(4, com.revrobotics.CANSparkLowLevel.MotorType.kBrushless);
 
 
-
-
-
-
-    
-
-
     }
 
     public void periodic() {
-
-       
-    
 
         boolean useJoystick;
         double leftJoyStick = controller.getLeftX();
@@ -71,9 +72,9 @@ public class DriveTrain extends SubsystemBase{
         leftTrigger = controller.getLeftTriggerAxis();
         rightTrigger = controller.getRightTriggerAxis();
         
-
-
-
+        // will add "constants." infront of turn and speed inhibitor once it's set up in constants  
+        steering *= turnSpeedInhibitor;
+        speed *= speedInhibitor;
 
     }
 
