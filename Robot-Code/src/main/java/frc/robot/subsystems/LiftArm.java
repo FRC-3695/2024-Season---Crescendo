6b2b136f334ac873fb3150bc03f881448833494f
Caroline Joy Kolody <cj@prodical.us>
2024-02-01 18:16:38 -0600

    Addition of Base lift arm code

---
+++ Robot-Code/src/main/java/frc/robot/subsystems/LiftArm.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ChainLift extends SubsystemBase {
+	
+}
