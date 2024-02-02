f7d8013c6d71378d7c5a9f6b321226098aee1727
ChrisNendick <155856069+ChrisNendick@users.noreply.github.com>
2024-02-01 18:45:16 -0600

    Update DriveTrain.java
    Catchup

--- Robot-Code/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,20 +4,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.RobotContainer;
+
 import com.revrobotics.RelativeEncoder;
-
-
-//Encoders
-//Motors
-//contols
-//speed and steering
-//
 
 
 
 public class DriveTrain extends SubsystemBase{
+
+    private static DriveTrain instance = null;
+    public static DriveTrain getInstance() {
+        if(instance == null)
+          instance = new DriveTrain();
+        return instance;
+      }
 
 // set up encoders for when we need to access them (currently unused)
     public static RelativeEncoder leftMasterEncoder;
@@ -33,20 +36,25 @@ public class DriveTrain extends SubsystemBase{
     double turnSpeedInhibitor = 0.3;
     double speedInhibitor = 0.3;
 
-
     public static CANSparkMax leftMaster;
     public static CANSparkMax leftSlave; 
     public static CANSparkMax rightMaster;
     public static CANSparkMax rightSlave;
 
 
-    public void Drive() {
+    public void DriveTrain() {
+
+    controller = RobotContainer.getDriver();
+
 
    //getting encoder values: 
     leftMasterEncoder = leftMaster.getEncoder();
     leftSlaveEncoder = leftMaster.getEncoder();
     rightMasterEncoder = rightMaster.getEncoder();
     rightSlaveEncoder = rightSlave.getEncoder();
+
+
+
 
 
 
@@ -75,6 +83,9 @@ public class DriveTrain extends SubsystemBase{
         double leftJoyStick = controller.getLeftX();
         double speed = MathUtil.applyDeadband(rightTrigger - leftTrigger, 0.1);
         double steering = MathUtil.applyDeadband(leftJoyStick, 0.1); 
+
+        SlewRateLimiter filter = new SlewRateLimiter(0.3);
+        filter.calculate(leftJoyStick);
     
         leftTrigger = controller.getLeftTriggerAxis();
         rightTrigger = controller.getRightTriggerAxis();
@@ -107,6 +118,7 @@ public class DriveTrain extends SubsystemBase{
         rightSlaveEncoder.setPosition(0);
     }
 
+ 
 
 
 
