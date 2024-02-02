f9fd2523053683481d845b7ae735491fe5944bce
Caroline Joy Kolody <cj@prodical.us>
2024-02-02 09:53:50 -0600

    Import RobotContainer.java into Robot.java
    To call a class you must first have it called upon when up stream from the file.

--- Robot-Code/src/main/java/frc/robot/Robot.java
+++ Robot-Code/src/main/java/frc/robot/Robot.java
@@ -1,4 +1,5 @@
 package frc.robot;
+import frc.robot.RobotContainer;                          //Import the RobotContainer into the Robot Timer Calls / Scheduler
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
