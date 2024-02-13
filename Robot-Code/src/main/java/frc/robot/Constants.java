2f194ac394aad8340283b9434a49b7490a5ec3a3
Caroline Joy Kolody <cj@prodical.us>
2024-02-13 16:31:44 -0600

    Base start of Constants file
    starting of constants file to simplify multi-location updating

---
+++ Robot-Code/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public final class Constants {
+    public static class robotConstants {                    // View is taken from rear of robot for sides
+        public static final int motor_leftFront  = 1;
+        public static final int motor_leftRear   = 2;
+        public static final int motor_rightFront = 3;
+        public static final int motor_rightRear  = 4;
+    }
+    public static class operatorConstants {
+        public static final int controller_xBox_driver = 0;
+        public static final int controller_xBox_manip  = 1;
+    }
+}
