fb69a3015c6baf0c261fd7ee7178e7fc227a60bb
Caroline Joy Kolody <cj@prodical.us>
2024-02-13 16:55:44 -0600

    Adding supporting Libraries and Attaching Constants file

--- Robot-Code/src/main/java/frc/robot/subsystems/drive.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/drive.java
@@ -1,5 +1,12 @@
 package frc.robot.subsystems;
+import frc.robot.Constants;           // Constants to pass can IDs along to subsystem
 
-public class drive {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;    // System to be extended by this subsystem
+// Lib for SparkMax Motor Controllers
+import com.revrobotics.CANSparkMax;                     // SparkMax
+import com.revrobotics.CANSparkLowLevel.MotorType;      // REVLib MotorType
+import com.revrobotics.RelativeEncoder;                 // Relative Encoder CAN Table
+
+public class drive extends SubsystemBase{
     
 }
