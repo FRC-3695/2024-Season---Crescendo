15ce93330efabcf7c21316666d794549d996070c
Caroline Joy Kolody <cj@prodical.us>
2024-02-13 18:17:25 -0600

    Motor mappings added into constants

--- Robot-Code/src/main/java/frc/robot/Constants.java
+++ Robot-Code/src/main/java/frc/robot/Constants.java
@@ -2,10 +2,22 @@ package frc.robot;
 
 public final class Constants {
     public static class robotConstants {                    // View is taken from rear of robot for sides
-        public static final int motor_leftFront  = 1;
-        public static final int motor_leftRear   = 2;
-        public static final int motor_rightFront = 3;
-        public static final int motor_rightRear  = 4;
+        // Drive Train Motor Controllers
+        public static final int motor_drive_leftFront    = 1;
+        public static final int motor_drive_leftRear     = 2;
+        public static final int motor_drive_rightFront   = 3;
+        public static final int motor_drive_rightRear    = 4;
+        // Lifter Motor Controllers
+        public static final int motor_lifterLeft         = 5;
+        public static final int motor_lifterRight        = 6;
+        // Manipulator Motor Controllers
+        public static final int motor_manipulator_feed   = 7;
+        public static final int motor_manipulator_posi   = 8;
+        // Shooter Motor Controllers
+        public static final int motor_shooter_leftFront  = 9;
+        public static final int motor_shooter_leftRear   = 10;
+        public static final int motor_shooter_rightFront = 11;
+        public static final int motor_shooter_rightRear  = 12;
     }
     public static class operatorConstants {
         public static final int controller_xBox_driver = 0;
