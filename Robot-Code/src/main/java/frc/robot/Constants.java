3e53d41a80eaa2e98592a0020e85293618626922
Caroline Joy Kolody <cj@prodical.us>
2024-02-14 15:48:03 -0600

    Added Sensor Constants

--- Robot-Code/src/main/java/frc/robot/Constants.java
+++ Robot-Code/src/main/java/frc/robot/Constants.java
@@ -3,24 +3,30 @@ package frc.robot;
 public final class Constants {
     public static class robotConstants {                    // View is taken from rear of robot for sides
         // Drive Train Motor Controllers
-        public static final int motor_drive_leftFront    = 1;
-        public static final int motor_drive_leftRear     = 2;
-        public static final int motor_drive_rightFront   = 3;
-        public static final int motor_drive_rightRear    = 4;
+        public static final int motor_drive_leftFront           = 1;
+        public static final int motor_drive_leftRear            = 2;
+        public static final int motor_drive_rightFront          = 3;
+        public static final int motor_drive_rightRear           = 4;
         // Lifter Motor Controllers
-        public static final int motor_lifterLeft         = 5;
-        public static final int motor_lifterRight        = 6;
+        public static final int motor_lifterLeft                = 5;
+        public static final int motor_lifterRight               = 6;
+        public static final int sensor_lifter_leftTop           = 0;
+        public static final int sensor_lifter_leftBot           = 0;
+        public static final int sensor_lifter_rightTop          = 0;
+        public static final int sensor_lifter_rightBot          = 0;
         // Manipulator Motor Controllers
-        public static final int motor_manipulator_feed   = 7;
-        public static final int motor_manipulator_posi   = 8;
+        public static final int motor_manipulator_feed          = 7;
+        public static final int motor_manipulator_posi          = 8;
+        public static final int sensor_mainpulator_deploy       = 0;
+        public static final int sensor_manupilator_retract      = 0;
         // Shooter Motor Controllers
-        public static final int motor_shooter_leftFront  = 9;
-        public static final int motor_shooter_leftRear   = 10;
-        public static final int motor_shooter_rightFront = 11;
-        public static final int motor_shooter_rightRear  = 12;
+        public static final int motor_shooter_leftFront         = 9;
+        public static final int motor_shooter_leftRear          = 10;
+        public static final int motor_shooter_rightFront        = 11;
+        public static final int motor_shooter_rightRear         = 12;
     }
     public static class operatorConstants {
-        public static final int controller_xBox_driver = 0;
-        public static final int controller_xBox_manip  = 1;
+        public static final int controller_xBox_driver          = 0;
+        public static final int controller_xBox_manip           = 1;
     }
 }
