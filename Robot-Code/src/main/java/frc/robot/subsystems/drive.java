9e7c5bf187b0175967142d3315273d28d5af7619
Caroline Joy Kolody <cj@prodical.us>
2024-02-15 12:47:38 -0600

    Start of drive subsystem
    Added declarations for motor controllers and start of functions for basic actions and needs

--- Robot-Code/src/main/java/frc/robot/subsystems/drive.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/drive.java
@@ -7,9 +7,28 @@ import com.revrobotics.CANSparkMax;                     // SparkMax
 import com.revrobotics.CANSparkBase.ExternalFollower;   // REVLib Follow Library
 import com.revrobotics.CANSparkLowLevel.MotorType;      // REVLib MotorType
 import com.revrobotics.RelativeEncoder;                 // Relative Encoder CAN Table
+// WPI Lib Functions
+import edu.wpi.first.math.geometry.Pose2d;              // Updates 2D field map for SmartDash
+
 
 public class drive extends SubsystemBase{
-    
+    // Defining Creation of drive motor controllers & encoders
+    private final CANSparkMax robot_drive_leftMaster  = new CANSparkMax(Constants.robotConstants.motor_drive_leftFront, MotorType.kBrushless);
+    private final CANSparkMax robot_drive_leftSlave   = new CANSparkMax(Constants.robotConstants.motor_drive_leftRear, MotorType.kBrushless);
+    private final CANSparkMax robot_drive_rightMaster = new CANSparkMax(Constants.robotConstants.motor_drive_rightFront, MotorType.kBrushless);
+    private final CANSparkMax robot_drive_rightSlave  = new CANSparkMax(Constants.robotConstants.motor_drive_rightRear, MotorType.kBrushless);
+
+    public void driveStart() {
+        setupFollowers();
+        zeroEncoders();
+    }
+    @Override
+    public void periodic() {
+
+    }
+    /*public Pose2d getPose() {
+
+    }*/
     private void setupFollowers() { // Within this function followers will be defined where a master and slave controller relationship will be defined
         // Reset Motors to factory base for core function settings
         robot_drive_leftMaster.restoreFactoryDefaults();
@@ -19,5 +38,8 @@ public class drive extends SubsystemBase{
         // Setting Followers
         robot_drive_leftSlave.follow(robot_drive_leftMaster);
         robot_drive_rightSlave.follow(robot_drive_rightMaster);
+    }
+    private void zeroEncoders() {
+
     }
 }
