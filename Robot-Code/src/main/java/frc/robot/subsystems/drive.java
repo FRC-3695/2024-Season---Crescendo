85c487521163706ba63fb073ba174cfbb1df3062
Caroline Joy Kolody <cj@prodical.us>
2024-02-15 15:20:16 -0600

    Formatting update
    Cleaning up formatting in drive file to allow for faster reading and editing

--- Robot-Code/src/main/java/frc/robot/subsystems/drive.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/drive.java
@@ -4,7 +4,7 @@ import frc.robot.Constants;           // Constants to pass can IDs along to subs
 import edu.wpi.first.wpilibj2.command.SubsystemBase;    // System to be extended by this subsystem
 // Lib for SparkMax Motor Controllers
 import com.revrobotics.CANSparkMax;                     // SparkMax
-import com.revrobotics.CANSparkBase.ExternalFollower;   // REVLib Follow Library
+import com.revrobotics.CANSparkBase.ExternalFollower;   // REVLib Follow
 import com.revrobotics.CANSparkLowLevel.MotorType;      // REVLib MotorType
 import com.revrobotics.RelativeEncoder;                 // REVLib Relative Encoder
 // WPI Lib Functions
@@ -13,11 +13,15 @@ import edu.wpi.first.wpilibj.Encoder;                   // Encoder Functions Sup
 
 
 public class drive extends SubsystemBase{
-    // Defining Creation of drive motor controllers & encoders
-    private final CANSparkMax robot_drive_leftMaster  = new CANSparkMax(Constants.robotConstants.motor_drive_leftFront, MotorType.kBrushless);
-    private final CANSparkMax robot_drive_leftSlave   = new CANSparkMax(Constants.robotConstants.motor_drive_leftRear, MotorType.kBrushless);
-    private final CANSparkMax robot_drive_rightMaster = new CANSparkMax(Constants.robotConstants.motor_drive_rightFront, MotorType.kBrushless);
-    private final CANSparkMax robot_drive_rightSlave  = new CANSparkMax(Constants.robotConstants.motor_drive_rightRear, MotorType.kBrushless);
+    // Defining Creation of drive motor controllers
+    private final CANSparkMax robot_drive_leftMaster = 
+        new CANSparkMax(Constants.robotConstants.motor_drive_leftFront, MotorType.kBrushless);
+    private final CANSparkMax robot_drive_leftSlave = 
+        new CANSparkMax(Constants.robotConstants.motor_drive_leftRear, MotorType.kBrushless);
+    private final CANSparkMax robot_drive_rightMaster = 
+        new CANSparkMax(Constants.robotConstants.motor_drive_rightFront, MotorType.kBrushless);
+    private final CANSparkMax robot_drive_rightSlave = 
+        new CANSparkMax(Constants.robotConstants.motor_drive_rightRear, MotorType.kBrushless);
     // Defining Encoders for Functions
     private RelativeEncoder robot_drive_encoderLeft =
         robot_drive_leftMaster.getEncoder();
