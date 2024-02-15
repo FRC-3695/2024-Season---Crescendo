f062d4accf6f0ad8b25373a14bfebdd5557df54a
Caroline Joy Kolody <cj@prodical.us>
2024-02-15 15:19:24 -0600

    Buildout of Encoders into Drive
    Adding support for grabbing encoder data from the leading motor controller in gearbox to enable acceleration and braking data along with positional data for targeting

--- Robot-Code/src/main/java/frc/robot/subsystems/drive.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/drive.java
@@ -6,9 +6,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;    // System to be extended
 import com.revrobotics.CANSparkMax;                     // SparkMax
 import com.revrobotics.CANSparkBase.ExternalFollower;   // REVLib Follow Library
 import com.revrobotics.CANSparkLowLevel.MotorType;      // REVLib MotorType
-import com.revrobotics.RelativeEncoder;                 // Relative Encoder CAN Table
+import com.revrobotics.RelativeEncoder;                 // REVLib Relative Encoder
 // WPI Lib Functions
 import edu.wpi.first.math.geometry.Pose2d;              // Updates 2D field map for SmartDash
+import edu.wpi.first.wpilibj.Encoder;                   // Encoder Functions Supplied
 
 
 public class drive extends SubsystemBase{
@@ -17,10 +18,15 @@ public class drive extends SubsystemBase{
     private final CANSparkMax robot_drive_leftSlave   = new CANSparkMax(Constants.robotConstants.motor_drive_leftRear, MotorType.kBrushless);
     private final CANSparkMax robot_drive_rightMaster = new CANSparkMax(Constants.robotConstants.motor_drive_rightFront, MotorType.kBrushless);
     private final CANSparkMax robot_drive_rightSlave  = new CANSparkMax(Constants.robotConstants.motor_drive_rightRear, MotorType.kBrushless);
+    // Defining Encoders for Functions
+    private RelativeEncoder robot_drive_encoderLeft =
+        robot_drive_leftMaster.getEncoder();
+    private RelativeEncoder robot_drive_encoderRight =
+        robot_drive_rightMaster.getEncoder();
 
     public void driveStart() {
         setupFollowers();
-        zeroEncoders();
+        startEncoders();
     }
     @Override
     public void periodic() {
@@ -39,7 +45,10 @@ public class drive extends SubsystemBase{
         robot_drive_leftSlave.follow(robot_drive_leftMaster);
         robot_drive_rightSlave.follow(robot_drive_rightMaster);
     }
-    private void zeroEncoders() {
-
+    private void startEncoders() {
+        robot_drive_encoderLeft.setPosition(0);
+        robot_drive_encoderRight.setPosition(0);
+        robot_drive_encoderLeft.getInverted();
+        robot_drive_encoderRight.getInverted();
     }
 }
