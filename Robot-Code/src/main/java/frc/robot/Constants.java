dc423d000622c5be5d1f54b31b4e274ec779d920
Caroline Joy Kolody <cj@prodical.us>
2024-02-16 11:41:15 -0600

    Updated to Match Robot Build
    # Robot has
    - 4 drive motors 2 to each side with a leader and follower
    - Manipulator with 1 positional motor and 1 feeder motor
    - 2 lifter arms each with 1 motor geared down with a 16:1 gearbox
    - Shooter uses 2 motors located left and right of the feed platform ideally to be controlled by pid loop

--- Robot-Code/src/main/java/frc/robot/Constants.java
+++ Robot-Code/src/main/java/frc/robot/Constants.java
@@ -10,20 +10,15 @@ public final class Constants {
         // Lifter Motor Controllers
         public static final int motor_lifterLeft                = 5;
         public static final int motor_lifterRight               = 6;
-        public static final int sensor_lifter_leftTop           = 0;
-        public static final int sensor_lifter_leftBot           = 0;
-        public static final int sensor_lifter_rightTop          = 0;
-        public static final int sensor_lifter_rightBot          = 0;
+        public static final int sensor_lifter_left              = 0;
+        public static final int sensor_lifter_right             = 0;
         // Manipulator Motor Controllers
         public static final int motor_manipulator_feed          = 7;
         public static final int motor_manipulator_posi          = 8;
-        public static final int sensor_mainpulator_deploy       = 0;
         public static final int sensor_manupilator_retract      = 0;
         // Shooter Motor Controllers
-        public static final int motor_shooter_leftFront         = 9;
-        public static final int motor_shooter_leftRear          = 10;
-        public static final int motor_shooter_rightFront        = 11;
-        public static final int motor_shooter_rightRear         = 12;
+        public static final int motor_shooter_left              = 9;
+        public static final int motor_shooter_right             = 10;
     }
     public static class operatorConstants {
         public static final int controller_xBox_driver          = 0;
