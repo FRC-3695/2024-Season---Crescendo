1106b3a5e9c667b09602670d61646597ec60d446
Caroline Joy Kolody <cj@prodical.us>
2024-02-16 12:28:39 -0600

    Start of Lifter Code

--- Robot-Code/src/main/java/frc/robot/subsystems/lifter.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/lifter.java
@@ -1,7 +1,33 @@
 package frc.robot.subsystems;
+import frc.robot.Constants;                             // Constants to pass can IDs along to subsystem
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;    // System to be extended by this subsystem
+// WPI Lib Functions
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+// Lib for SparkMax Motor Controllers
+import com.revrobotics.CANSparkMax;                     // SparkMax
+import com.revrobotics.CANSparkLowLevel.MotorType;      // REVLib MotorType
+import com.revrobotics.RelativeEncoder;                 // REVLib Relative Encoder
 
 public class lifter extends SubsystemBase {
+    // Defining Motor Controllers
+    private final CANSparkMax motor_lifter_left =
+        new CANSparkMax(Constants.robotConstants.motor_lifterLeft, MotorType.kBrushless);
+    private final CANSparkMax motor_lifter_right =
+        new CANSparkMax(Constants.robotConstants.motor_lifterRight, MotorType.kBrushless)
+    // Defining Sensors
+    private final RelativeEncoder encoder_lifter_left =
+        motor_lifter_left.getEncoder();
+    private final RelativeEncoder encoder_lifter_right =
+        motor_lifter_right.getEncoder();
     
+
+    public lifter() {
+
+    }
+    @Override
+    public void periodic() {
+        
+    }
 }
