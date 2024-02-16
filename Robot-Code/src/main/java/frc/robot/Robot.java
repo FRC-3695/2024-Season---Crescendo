19703200cfe9b5ec92ff7b8c33f08fcd8f9e2729
Caroline Joy Kolody <cj@prodical.us>
2024-02-16 11:50:30 -0600

    Previsions to driveSys and execution of subsystem

--- Robot-Code/src/main/java/frc/robot/Robot.java
+++ Robot-Code/src/main/java/frc/robot/Robot.java
@@ -5,13 +5,16 @@
 package frc.robot;
 import frc.robot.Constants;
 import frc.robot.BuildConstants;
+import frc.robot.subsystems.driveSys;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 // Adds Smart Dashboard Capabilities for Autonomous chooser
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 public class Robot extends TimedRobot {
   // Creation of Autonomous functions IDs and Varriables
@@ -23,8 +26,9 @@ public class Robot extends TimedRobot {
   private final SendableChooser<String> dash_autoOptions = new SendableChooser<>();
 
   private Command m_autonomousCommand;
-
+  private Command drivePeriodic;
   private RobotContainer m_robotContainer;
+  private driveSys robot_code_drive;
 
   @Override
   public void robotInit() {
@@ -50,6 +54,9 @@ public class Robot extends TimedRobot {
     SmartDashboard.putData("Auto choices", dash_autoOptions);
 
     m_robotContainer = new RobotContainer();
+    // Decleration of robot functions
+    robot_code_drive = new driveSys();
+
   }
 
   @Override
@@ -68,12 +75,12 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    dash_autoSelected = dash_autoOptions.getSelected();
-    /*m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    dash_autoSelected = dash_autoOptions.getSelected(); //Gets Selected Auto Command from DriverStation
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
-    }*/
+    }
   }
 
   @Override
