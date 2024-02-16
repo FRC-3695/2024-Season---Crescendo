c9e1ce69e05a3e944dd302589da533493bda6d06
Caroline Joy Kolody <cj@prodical.us>
2024-02-16 12:27:11 -0600

    Addition of Port Type Labeling

--- Robot-Code/src/main/java/frc/robot/Constants.java
+++ Robot-Code/src/main/java/frc/robot/Constants.java
@@ -3,25 +3,26 @@ package frc.robot;
 public final class Constants {
     public static class robotConstants {                    // View is taken from rear of robot for sides
         // Drive Train Motor Controllers
-        public static final int motor_drive_leftFront           = 1;
-        public static final int motor_drive_leftRear            = 2;
-        public static final int motor_drive_rightFront          = 3;
-        public static final int motor_drive_rightRear           = 4;
+        public static final int motor_drive_leftFront           = 1;  // CAN ID
+        public static final int motor_drive_leftRear            = 2;  // CAN ID
+        public static final int motor_drive_rightFront          = 3;  // CAN ID
+        public static final int motor_drive_rightRear           = 4;  // CAN ID
         // Lifter Motor Controllers
-        public static final int motor_lifterLeft                = 5;
-        public static final int motor_lifterRight               = 6;
-        public static final int sensor_lifter_left              = 0;
-        public static final int sensor_lifter_right             = 0;
+        public static final int motor_lifterLeft                = 5;  // CAN ID
+        public static final int motor_lifterRight               = 6;  // CAN ID
+        public static final int sensor_lifter_left              = 0;  // Digital IO
+        public static final int sensor_lifter_right             = 1;  // Digital IO
         // Manipulator Motor Controllers
-        public static final int motor_manipulator_feed          = 7;
-        public static final int motor_manipulator_posi          = 8;
-        public static final int sensor_manupilator_retract      = 0;
+        public static final int motor_manipulator_feed          = 7;  // CAN ID
+        public static final int motor_manipulator_posi          = 8;  // CAN ID
+        public static final int sensor_manupilator_retract      = 2;  // Digital IO
         // Shooter Motor Controllers
-        public static final int motor_shooter_left              = 9;
-        public static final int motor_shooter_right             = 10;
+        public static final int motor_shooter_left              = 9;  // CAN ID
+        public static final int motor_shooter_right             = 10; // CAN ID
     }
     public static class operatorConstants {
-        public static final int controller_xBox_driver          = 0;
-        public static final int controller_xBox_manip           = 1;
+        public static final int controller_xBox_driver          = 0;  // DS USB ID
+        public static final int controller_xBox_manip           = 1;  // DS USB ID
+    }
     }
 }
