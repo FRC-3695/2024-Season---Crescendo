86ab2e0e5c09308d72a9b6f9774917207066885b
Caroline Joy Kolody <cj@prodical.us>
2024-02-16 18:13:54 -0600

    Subsystem name change from manipulator to manipulatorShooter

--- Robot-Code/src/main/java/frc/robot/subsystems/manipulatorShooter.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/manipulatorShooter.java
@@ -2,6 +2,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class manipulator extends SubsystemBase {
+public class manipulatorShooter extends SubsystemBase {
     
 }
