1a99d9c5dde01a778ea94f1414ae85fc64892ac0
Caroline Joy Kolody <cj@prodical.us>
2024-02-20 18:40:31 -0600

    Reapplying Chris's Work

--- Robot-Code/src/main/java/frc/robot/subsystems/vision.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/vision.java
@@ -1,7 +1,23 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
-public class vision extends SubsystemBase {
-    
-}
+public class vision {
+    public vision() {
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("tx");
+        NetworkTableEntry ty = table.getEntry("ty");
+        NetworkTableEntry ta = table.getEntry("ta");
+
+        double x = tx.getDouble(0.0);
+        double y = ty.getDouble(0.0); 
+        double area = ta.getDouble(0.0);
+
+        SmartDashboard.putNumber("LimelightX", x);
+        SmartDashboard.putNumber("LimelightY", y);
+        SmartDashboard.putNumber("LimelightArea", area);
+    }
+} 
