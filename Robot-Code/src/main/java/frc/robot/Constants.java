90b3f8d6b6d4488d6611d05d08afbef3a88ef150
Caroline Joy Kolody <cj@prodical.us>
2024-03-01 17:15:37 -0600

    Added clearer notes to Constants
    * PowerHub to PowerHub Port - Denoting when reading that we are referencing PowerHub Port that the device is plugged into
    
    Signed-off-by: Caroline Joy Kolody <cj@prodical.us>

--- Robot-Code/src/main/java/frc/robot/Constants.java
+++ Robot-Code/src/main/java/frc/robot/Constants.java
@@ -4,31 +4,31 @@ public final class Constants {
     public static class IDs {                    // View is taken from rear of robot for sides
         // Drive Train Motor Controllers
         public static final int drive_leftFront_motor           = 1;  // CAN ID
-        public static final int drive_leftFront_powerBus        = 0;  // PowerHub
+        public static final int drive_leftFront_powerBus        = 0;  // PowerHub Port
         public static final int drive_leftRear_motor            = 2;  // CAN ID
-        public static final int drive_leftRear_powerBus         = 0;  // PowerHub
+        public static final int drive_leftRear_powerBus         = 0;  // PowerHub Port
         public static final int drive_rightFront_motor          = 3;  // CAN ID
-        public static final int drive_rightFront_powerBus       = 0;  // PowerHub
+        public static final int drive_rightFront_powerBus       = 0;  // PowerHub Port
         public static final int drive_rightRear_motor           = 4;  // CAN ID
-        public static final int drive_rightRear_powerBus        = 0;  // PowerHub
+        public static final int drive_rightRear_powerBus        = 0;  // PowerHub Port
         // Lifter Motor Controllers
         public static final int lifter_left_motor               = 5;  // CAN ID
-        public static final int lifter_left_powerBus            = 0;  // PowerHub
+        public static final int lifter_left_powerBus            = 0;  // PowerHub Port
         public static final int lifter_right_motor              = 6;  // CAN ID
-        public static final int lifter_right_powerBus           = 0;  // PowerHub
+        public static final int lifter_right_powerBus           = 0;  // PowerHub Port
         public static final int lifter_left_sensor              = 0;  // Digital IO
         public static final int lifter_right_sensor             = 1;  // Digital IO
         // Manipulator Motor Controllers
         public static final int manipulator_feed_motor          = 7;  // CAN ID
-        public static final int manipulator_feed_powerBus       = 0;  // PowerHub
+        public static final int manipulator_feed_powerBus       = 0;  // PowerHub Port
         public static final int manipulator_posi_motor          = 8;  // CAN ID
-        public static final int manipulator_posi_powerBus       = 0;  // PowerHub
+        public static final int manipulator_posi_powerBus       = 0;  // PowerHub Port
         public static final int manupilator_retract_sensor      = 2;  // Digital IO
         // Shooter Motor Controllers
         public static final int shooter_left_motor              = 9;  // CAN ID
-        public static final int shooter_left_powerBus           = 0;  // PowerHub
+        public static final int shooter_left_powerBus           = 0;  // PowerHub Port
         public static final int shooter_right_motor             = 10; // CAN ID
-        public static final int shooter_right_powerBus          = 0;  // PowerHub
+        public static final int shooter_right_powerBus          = 0;  // PowerHub Port
     }
     public static class operator {
         public static final int controller_xBox_driver          = 0;            // DS USB ID
