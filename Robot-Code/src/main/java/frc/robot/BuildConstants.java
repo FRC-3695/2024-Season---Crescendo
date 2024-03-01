ee92d301b968510b9d5b763bdb9876661e11640c
Foximus Prime 3695 - SYS01 <2707036@y115.org>
2024-02-29 19:58:49 -0600

    setting up encoders
    getting encoder values

--- Robot-Code/src/main/java/frc/robot/BuildConstants.java
+++ Robot-Code/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 121;
-  public static final String GIT_SHA = "864c5770a193c3ecee21331cb8d5f6ef08a2fec8";
-  public static final String GIT_DATE = "2024-02-27 15:14:23 CST";
+  public static final int GIT_REVISION = 122;
+  public static final String GIT_SHA = "4f886a7cd1421fa5633f3be8652dc8c67da9990f";
+  public static final String GIT_DATE = "2024-02-27 15:58:31 CST";
   public static final String GIT_BRANCH = "Rescue-Plan";
-  public static final String BUILD_DATE = "2024-02-27 15:32:18 CST";
-  public static final long BUILD_UNIX_TIME = 1709069538183L;
+  public static final String BUILD_DATE = "2024-02-29 19:51:55 CST";
+  public static final long BUILD_UNIX_TIME = 1709257915825L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- Robot-Code/src/main/java/frc/robot/subsystems/vision.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/vision.java
@@ -2,12 +2,36 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;    // System to be extended by this subsystem
 
+
+
+
+
 public class vision extends SubsystemBase{
+
+
+    static CANSparkMax leftMaster;
+    static CANSparkMax leftSlave;
+    static CANSparkMax rightMaster;
+    static CANSparkMax rightSlave;
+
+    RelativeEncoder leftMasterEncoder;
+    RelativeEncoder leftSlaveEncoder;
+    RelativeEncoder rightMasterEncoder;
+    RelativeEncoder rightSlaveEncoder;
+
+
+   
+
+
     public void periodic() {
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
         NetworkTableEntry tx = table.getEntry("tx");
@@ -21,16 +45,28 @@ public class vision extends SubsystemBase{
         SmartDashboard.putNumber("LimelightY", y);
         SmartDashboard.putNumber("LimelightArea", area);
 
-       //Basic Functionality Tests
-        if (SmartDashboard.getNumber("Limelightx", x) == Constants.vision.targeting_april_alignment[4][0] && y == Constants.vision.targeting_april_alignment[4][2]) {
-            System.out.println("works");        
+        
+        leftMasterEncoder = leftMaster.getEncoder();
+        leftSlaveEncoder = leftSlave.getEncoder();
+        rightMasterEncoder = rightMaster.getEncoder();
+        rightSlaveEncoder = rightSlave.getEncoder();
+    
+
+        //Basic Functionality Tests
+       //this works: first number is y, second number is x
+        if (SmartDashboard.getNumber("Limelightx", x) == Constants.vision.targeting_april_alignment[4][0]) {
+            System.out.println("DictionaryWorks!");        
         }
+
         if (SmartDashboard.getNumber("Limelightx", x) <= 7 && SmartDashboard.getNumber("Limelightx", x) >= 6) {
              System.out.println("Works!");
         }
         if (SmartDashboard.getNumber("Limelightx", y) <= 7 && SmartDashboard.getNumber("Limelightx", y) >= 6) {
              System.out.println("Works!");
         }
-        
     }
+
+      
+        
+    
 } 
