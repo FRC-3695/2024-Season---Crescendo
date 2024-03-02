f48e0139ca48487ea6c238012ed5c677f8b4d78e
Caroline Joy Kolody <cj@prodical.us>
2024-03-02 12:09:08 -0600

    EOD 3/1/2024
    Signed-off-by: Caroline Joy Kolody <cj@prodical.us>

--- Robot-Code/src/main/java/frc/robot/subsystems/manipulator.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/manipulator.java
@@ -39,37 +39,64 @@ public class manipulator extends SubsystemBase {
         new DigitalInput(Constants.IDs.manupilator_retract_sensor);
     
     public manipulator () { // Core Function
-
+        intakeSetup();
+        intakeZero();
+        shooterSetup();
+        dashboardSetup();
     }
     @Override
     public void periodic () {
+        dashboardUpdate();
+    }
+    private void intakeDeploy() {  // Deploys intake 
 
     }
-    private static void intakeDeploy() {  // Deploys intake 
+    private void intakeRetract() {  // Retracts intake
 
     }
-    private static void intakeRetract() {  // Retracts intake
+    private void intakeFeed() {  // Runs feeder on intake until ring is pulled in
 
     }
-    private static void intakeFeed() {  // Runs feeder on intake until ring is pulled in
+    private void intakeClear() {  // Reverses feeder on intake and completes a couple clearing cycles
 
     }
-    private static void intakeClear() {  // Reverses feeder on intake and completes a couple clearing cycles
+    private void shooterSpinUp() {  // Spins up motor to set RPM with PID
 
     }
-    private static void shooterSpinUp() {  // Spins up motor to set RPM with PID
+    private void shooterSpinDown() {  // Spins down gracefully
 
     }
-    private static void shooterSpinDown() {  // Spins down gracefully
+    private void intakeSetup() {  // Sets up parameters for intake sensors and motor controllers
+        // Zeroing motor settings
+        intake_position_motor.restoreFactoryDefaults();
+        intake_feeder_motor.restoreFactoryDefaults();
+        // Setting PID Loop Feed Back Devices
+        
+    }
+    private void intakeZero() {  // Zeros home for intake
 
     }
-    private static void intakeSetup() {  // Sets up parameters for intake sensors and motor controllers
+    private void shooterSetup() {  // Sets up parameters for shooter motor controllers
+        // Zeroing motor settings
+        shooter_left_motor.restoreFactoryDefaults();
+        shooter_right_motor.restoreFactoryDefaults();
+        // Reversing correct motor for forward operation
+        shooter_left_motor.setInverted(Constants.shooter.rev_left);
+        shooter_right_motor.setInverted(Constants.shooter.rev_right);
+        // Setting PID Loop Feed Back Devices
+        shooter_left_PID.setFeedbackDevice(shooter_left_encoder);
+        shooter_right_PID.setFeedbackDevice(shooter_right_encoder);
+        // Loading Base PIDs into Motors
+        shooter_left_PID.setP(Constants.shooter.tuning_P);            shooter_right_PID.setP(Constants.shooter.tuning_P);
+        shooter_left_PID.setI(Constants.shooter.tuning_I);            shooter_right_PID.setI(Constants.shooter.tuning_I);
+        shooter_left_PID.setD(Constants.shooter.tuning_D);            shooter_right_PID.setD(Constants.shooter.tuning_D);
+        shooter_left_PID.setIZone(Constants.shooter.tuning_Iz);       shooter_right_PID.setIZone(Constants.shooter.tuning_Iz);
+        shooter_left_PID.setFF(Constants.shooter.tuning_FF);          shooter_right_PID.setFF(Constants.shooter.tuning_FF);
+    }
+    private void dashboardSetup() {
 
     }
-    private static void intakeZero() {  // Zeros home for intake
-
-    }
-    private static void shooterSetup() {  // Sets up parameters for shooter motor controllers
+    private void dashboardUpdate() {
 
     }
 }
