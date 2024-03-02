c3ffae3c638d8bc470d1ed5a09d04dddaabeda6d
Caroline Joy Kolody <cj@prodical.us>
2024-03-02 12:08:53 -0600

    EOD 2/29/2024
    Signed-off-by: Caroline Joy Kolody <cj@prodical.us>

--- Robot-Code/src/main/java/frc/robot/subsystems/manipulator.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/manipulator.java
@@ -7,10 +7,37 @@ import com.revrobotics.CANSparkLowLevel.MotorType;      // REVLib MotorType
 import com.revrobotics.RelativeEncoder;                 // REVLib Relative Encoder
 import com.revrobotics.SparkPIDController;              // REVLib SparkPID Control
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class manipulator extends SubsystemBase {
-
+    // Defining Motor Controllers
+    private final CANSparkMax intake_position_motor =
+        new CANSparkMax(Constants.IDs.manipulator_posi_motor, MotorType.kBrushless);
+    private final CANSparkMax intake_feeder_motor =
+        new CANSparkMax(Constants.IDs.manipulator_posi_motor, MotorType.kBrushed);
+    private final CANSparkMax shooter_left_motor =
+        new CANSparkMax(Constants.IDs.shooter_left_motor, MotorType.kBrushless);
+    private final CANSparkMax shooter_right_motor =
+        new CANSparkMax(Constants.IDs.shooter_right_motor, MotorType.kBrushless);
+    // Defining Encoders
+    private final RelativeEncoder intake_position_encoder =
+        intake_position_motor.getEncoder();
+    private final RelativeEncoder shooter_left_encoder =
+        shooter_left_motor.getEncoder();
+    private final RelativeEncoder shooter_right_encoder =
+        shooter_right_motor.getEncoder();
+    // Defining PIDs
+    private final SparkPIDController intake_position_PID =
+        intake_position_motor.getPIDController();
+    private final SparkPIDController shooter_left_PID =
+        shooter_left_motor.getPIDController();
+    private final SparkPIDController shooter_right_PID =
+        shooter_right_motor.getPIDController();
+    // Defining Digital IO
+    private final DigitalInput intake_home_DIO =
+        new DigitalInput(Constants.IDs.manupilator_retract_sensor);
+    
     public manipulator () { // Core Function
 
     }
